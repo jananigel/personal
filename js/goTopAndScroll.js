cc3bd015831711cae8bbc9fac4b3ca79a5886276
Kyle Chang <jananigel@hotmail.com>
2017-05-22 15:03:25 +0800

    update cv UI

--- js/goTopAndScroll.js
+++ js/goTopAndScroll.js
@@ -1,12 +1,12 @@
 $(function(){
-	$('.resume').css({opacity: 0});
-		$('.introSpace').css({opacity: 0});
-		$('.portfolio').css({opacity: 0});
+	//$('.resume').css({opacity: 0});
+		//$('.introSpace').css({opacity: 0});
+		//$('.portfolio').css({opacity: 0});
 		$('.goTop').css({display:'none'});
 		$(window).scroll(function(){
 	   /* var scrollPos = $(document).scrollTop();
 	    console.log(scrollPos);*/
-	    if($($(window).scrollTop() + $(window).height() > $('.resume').offset().top + $('.resume').outerHeight())){
+	    /*if($($(window).scrollTop() + $(window).height() > $('.resume').offset().top + $('.resume').outerHeight())){
 	    	$('.resume').animate({
 	    		opacity: '1'
 	    	},800,function(){
@@ -20,7 +20,7 @@ $(function(){
 			    	})
 			    }
 	    	});
-	    };
+	    };*/
 	    //gotop
 	    if($(window).scrollTop() > $('.banner').offset().top){
 	    	$('.goTop').fadeIn();
